8d5ab2ded7d473de07a096209e7cedc3d9a83b3f
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2019-08-26 09:43:06 -0700

    adding initial screen with select for account

---
+++ nerdlets/neon-nerdlet/box-spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class BoxSpinner extends React.Component {
+  render() {
+    return (
+      <div className="spinner">
+        <div className="side left"></div>
+        <div className="side top"></div>
+        <div className="side right"></div>
+        <div className="side bottom"></div>
+      </div>
+    )
+  }
+}
--- nerdlets/neon-nerdlet/index.js
+++ nerdlets/neon-nerdlet/index.js
@@ -1,15 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { AccountsQuery } from 'nr1'
+
+import BoxSpinner from './box-spinner.js'
+import Select from './select.js'
 
 export default class NeonNerdlet extends React.Component {
-    static propTypes = {        
-        nerdletUrlState: PropTypes.object,
-        launcherUrlState: PropTypes.object,
-        width: PropTypes.number,
-        height: PropTypes.number,
-    }
+  static propTypes = {
+    nerdletUrlState: PropTypes.object,
+    launcherUrlState: PropTypes.object,
+    width: PropTypes.number,
+    height: PropTypes.number,
+  }
 
-    render() {
-        return <h1>Hello, neon-nerdlet Nerdlet!</h1>;
+  constructor(props) {
+    super(props)
+
+    this.parseAccounts = this.parseAccounts.bind(this)
+    this.accountChange = this.accountChange.bind(this)
+
+    this.state = {
+      accounts: [],
+      account: []
     }
+  }
+
+  componentDidMount() {
+    AccountsQuery.query().then(this.parseAccounts)
+  }
+
+  parseAccounts(res) {
+    if ('data' in res && 'actor' in res.data && 'accounts' in res.data.actor) {
+      const accounts = res.data.actor.accounts.map(a => ({id: a.id, value: a.name}))
+      this.setState({
+        accounts: accounts
+      })
+    }
+  }
+
+  accountChange(account) {
+    console.log('selected...', account)
+    this.setState({account: account})
+  }
+
+  render() {
+    const { accounts, account, boards, board } = this.state
+    
+    return (
+      <div className="container">
+        <Select label="Account" placeholder="Select an account" values={account} options={accounts} onChange={this.accountChange} />
+        {!account.length && <BoxSpinner />}
+      </div>
+    )
+  }
 }
