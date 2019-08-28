c80b68e07bf8126c07acd07ac178ac512cf77d43
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2019-08-28 09:00:37 -0700

    moved admin to its own component

---
+++ nerdlets/neon-nerdlet/board-admin.js
@@ -0,0 +1,179 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Icon, Modal, Tabs, TabsItem, TextField, Button } from 'nr1'
+
+import Select from './select'
+
+export default class BoardAdmin extends React.Component {
+  static propTypes = {
+    rows: PropTypes.array,
+    cols: PropTypes.array,
+    cells: PropTypes.array,
+    onSave: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      rowName: '',
+      rowForCell: [],
+      colName: '',
+      colForCell: [],
+      cells: [],
+      cellType: '',
+      policyName: '',
+      attributeName: '',
+      isType: '',
+      valueName: '',
+
+    }
+
+    this.textUpdated = this.textUpdated.bind(this)
+    this.cellSelect = this.cellSelect.bind(this)
+    this.optionChange = this.optionChange.bind(this)
+    this.addData = this.addData.bind(this)
+    this.persistData = this.persistData.bind(this)
+  }
+
+  textUpdated(e, type) {
+    const o = {}
+    o[type + 'Name'] = e.target.value
+    this.setState(o)
+  }
+
+  cellSelect(val, type) {
+    const o = {}
+    o[type + 'ForCell'] = val
+    this.setState(o)
+  }
+
+  optionChange(val, type) {
+    const o = {}
+    o[type + 'Type'] = val
+    this.setState(o)
+  }
+
+  addData(type) {
+    const { rowName, rowForCell, colName, colForCell, cellType, policyName, attributeName, isType, valueName } = this.state
+    const { rows, cols, cells } = this.props
+
+    if (type === 'row') {
+      if (rows.filter(r => (r === rowName)).length) return
+      rows.push(rowName)
+      this.setState({
+        rows: rows,
+        rowName: ''
+      }, this.persistData(rows, cols, cells))
+    } else if (type === 'col') {
+      if (cols.filter(c => (c === colName)).length) return
+      cols.push(colName)
+      this.setState({
+        cols: cols,
+        colName: ''
+      }, this.persistData(rows, cols, cells))
+    } else if (type === 'cell') {
+      if (cells.reduce((a, c) => (a || (c.row === rowForCell[0] && c.col === colForCell[0])), false)) return
+      if (cellType === 'alert') {
+        if (!policyName) return
+        cells.push({row: rowForCell[0], col: colForCell[0], policy: policyName})
+      } else if (cellType === 'data') {
+        if (!attributeName) return
+        const deets = {}
+        const attr = attributeName.split(/ as /i)
+        if (attr.length === 2) deets.name = attr[1].replace(/[^\w]/gi, '')
+        if (attr.length === 1 || attr.length === 2) {
+          const attrParts = attr[0].split('(')
+          deets.key = (attrParts[1] || attrParts[0]).replace(/[^\w]/gi, '')
+          deets.func = (attrParts.length === 2) ? attrParts[0].replace(/[^\w]/gi, '') : 'latest'
+        }
+        if (!('name' in deets)) deets.name = deets.func + '_' + deets.key
+        deets.str = deets.func + '(' + deets.key + ')' + ' AS ' + deets.name
+        deets.is = isType
+        deets.value = valueName
+        cells.push({row: rowForCell[0], col: colForCell[0], attribute: attributeName, details: deets})
+      }
+      this.setState({
+        cells: cells,
+        rowForCell: [],
+        colForCell: [],
+        cellType: '',
+        policyName: '',
+        attributeName: '',
+        isType: '',
+        valueName: ''
+      }, this.persistData(rows, cols, cells))
+    }
+  }
+
+  persistData(rows, cols, cells) {
+    const { onSave } = this.props
+    if (onSave) onSave(rows, cols, cells)
+  }
+
+  render() {
+    const { rowName, rowForCell, colName, colForCell, cellType, policyName, attributeName, isType, valueName } = this.state
+    const { rows, cols, cells } = this.props
+
+    return (
+      <div>
+        <h3>Board Details</h3>
+        <Tabs>
+          <TabsItem itemKey="tab-rows" label="Rows">
+            <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
+              <TextField label="Title" placeholder="" onChange={(e) => this.textUpdated(e, 'row')} value={rowName} />
+              <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('row')}>Add</Button>
+            </div>
+          </TabsItem>
+          <TabsItem itemKey="tab-cols" label="Columns">
+            <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
+              <TextField label="Title" placeholder="" onChange={(e) => this.textUpdated(e, 'col')} value={colName} />
+              <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('col')}>Add</Button>
+            </div>
+          </TabsItem>
+          <TabsItem itemKey="tab-cells" label="Cells">
+            <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
+              <Select label="Row" placeholder="" values={rowForCell || []} options={rows.map(r => ({value: r}))} onChange={(val) => this.cellSelect(val, 'row')} />
+              <Select label="Column" placeholder="" values={colForCell || []} options={cols.map(c => ({value: c}))} onChange={(val) => this.cellSelect(val, 'col')} />
+              <ul class="segmented-control">
+                  <li class="segmented-control__item">
+                      <input class="segmented-control__input" type="radio" name="cell-alert" value="alert" checked={cellType === 'alert'} />
+                      <label class="segmented-control__label" for="cell-alert"  onClick={(e) => this.optionChange('alert', 'cell')}>NR Alert</label>
+                  </li>
+                  <li class="segmented-control__item">
+                      <input class="segmented-control__input" type="radio" name="cell-data" value="data" checked={cellType === 'data'} />
+                      <label class="segmented-control__label" for="cell-data" onClick={(e) => this.optionChange('data', 'cell')}>NR Data</label>
+                  </li>
+              </ul>
+              {cellType === 'alert' && <TextField label="Alert Policy" placeholder="" onChange={(e) => this.textUpdated(e, 'policy')} value={policyName} />}
+              {cellType === 'data' && (
+                <div>
+                  <TextField label="Attribute Name" placeholder="" onChange={(e) => this.textUpdated(e, 'attribute')} value={attributeName} />
+                  <div style={{display: 'grid', gridTemplateColumns: '3fr 1fr', gridGap: '.5em', alignItems: 'center'}}>
+                    <ul class="segmented-control">
+                        <li class="segmented-control__item">
+                            <input class="segmented-control__input" type="radio" name="is-less" value="alert" checked={isType === 'less'} />
+                            <label class="segmented-control__label" for="is-more"  onClick={(e) => this.optionChange('less', 'is')}>less than</label>
+                        </li>
+                        <li class="segmented-control__item">
+                            <input class="segmented-control__input" type="radio" name="is-equal" value="equal" checked={isType === 'equal'} />
+                            <label class="segmented-control__label" for="is-equal" onClick={(e) => this.optionChange('equal', 'is')}>equals</label>
+                        </li>
+                        <li class="segmented-control__item">
+                            <input class="segmented-control__input" type="radio" name="is-more" value="alert" checked={isType === 'more'} />
+                            <label class="segmented-control__label" for="is-more"  onClick={(e) => this.optionChange('more', 'is')}>greater than</label>
+                        </li>
+                    </ul>
+                    <TextField label="Value" placeholder="" onChange={(e) => this.textUpdated(e, 'value')} value={valueName} style={{marginBottom: '.75em'}} />
+                  </div>
+                </div>
+              )}
+              <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('cell')}>Add</Button>
+            </div>
+          </TabsItem>
+        </Tabs>
+      </div>
+    )
+  }
+}
--- nerdlets/neon-nerdlet/board.js
+++ nerdlets/neon-nerdlet/board.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import { launcher, nerdlet, AccountStorageQuery, AccountStorageMutation, NerdGraphQuery, Icon, Modal, Tabs, TabsItem, TextField, Button } from 'nr1'
 
-import Select from './select.js'
+import BoardAdmin from './board-admin'
 
 export default class Board extends React.Component {
   static propTypes = {
@@ -19,17 +19,8 @@ export default class Board extends React.Component {
     this.state = {
       data: {},
       rows: [],
-      rowName: '',
-      rowForCell: [],
       cols: [],
-      colName: '',
-      colForCell: [],
       cells: [],
-      cellType: '',
-      policyName: '',
-      attributeName: '',
-      isType: '',
-      valueName: '',
       alerts: {},
       board:  ((props || {}).board || {}).name || '',
       event: ((props || {}).board || {}).event,
@@ -40,10 +31,7 @@ export default class Board extends React.Component {
     this.getBoard = this.getBoard.bind(this)
     this.openAdmin = this.openAdmin.bind(this)
     this.closeAdmin = this.closeAdmin.bind(this)
-    this.textUpdated = this.textUpdated.bind(this)
-    this.cellSelect = this.cellSelect.bind(this)
-    this.optionChange = this.optionChange.bind(this)
-    this.addData = this.addData.bind(this)
+
     this.persistData = this.persistData.bind(this)
     this.fetchAlertStatuses = this.fetchAlertStatuses.bind(this)
     this.parseAlertStatuses = this.parseAlertStatuses.bind(this)
@@ -96,88 +84,6 @@ export default class Board extends React.Component {
 
   closeAdmin() {
     this.setState({ modalHidden: true })
-  }
-
-  textUpdated(e, type) {
-    const o = {}
-    o[type + 'Name'] = e.target.value
-    this.setState(o)
-  }
-
-  cellSelect(val, type) {
-    const o = {}
-    o[type + 'ForCell'] = val
-    this.setState(o)
-  }
-
-  optionChange(val, type) {
-    const o = {}
-    o[type + 'Type'] = val
-    this.setState(o)
-  }
-
-  addData(type) {
-    const { rows, rowName, rowForCell, cols, colName, colForCell, cells, cellType, policyName, attributeName, isType, valueName } = this.state
-
-    if (type === 'row') {
-      if (rows.filter(r => (r === rowName)).length) return
-      rows.push(rowName)
-      this.setState({
-        rows: rows,
-        rowName: ''
-      }, this.persistData)
-    } else if (type === 'col') {
-      if (cols.filter(c => (c === colName)).length) return
-      cols.push(colName)
-      this.setState({
-        cols: cols,
-        colName: ''
-      }, this.persistData)
-    } else if (type === 'cell') {
-      if (cells.reduce((a, c) => (a || (c.row === rowForCell[0] && c.col === colForCell[0])), false)) return
-      if (cellType === 'alert') {
-        if (!policyName) return
-        cells.push({row: rowForCell[0], col: colForCell[0], policy: policyName})
-      } else if (cellType === 'data') {
-        if (!attributeName) return
-        const deets = {}
-        const attr = attributeName.split(/ as /i)
-        if (attr.length === 2) deets.name = attr[1].replace(/[^\w]/gi, '')
-        if (attr.length === 1 || attr.length === 2) {
-          const attrParts = attr[0].split('(')
-          deets.key = (attrParts[1] || attrParts[0]).replace(/[^\w]/gi, '')
-          deets.func = (attrParts.length === 2) ? attrParts[0].replace(/[^\w]/gi, '') : 'latest'
-        }
-        if (!('name' in deets)) deets.name = deets.func + '_' + deets.key
-        deets.str = deets.func + '(' + deets.key + ')' + ' AS ' + deets.name
-        deets.is = isType
-        deets.value = valueName
-        cells.push({row: rowForCell[0], col: colForCell[0], attribute: attributeName, details: deets})
-      }
-      this.setState({
-        cells: cells,
-        rowForCell: [],
-        colForCell: [],
-        cellType: '',
-        policyName: '',
-        attributeName: '',
-        isType: '',
-        valueName: ''
-      }, this.persistData)
-    }
-  }
-
-  persistData() {
-    const { rows, cols, cells } = this.state
-    const { board, accountId } = this.props
-
-    AccountStorageMutation.mutate({
-      actionType: AccountStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
-      collection: 'neondb-' + board.id,
-      accountId: accountId,
-      documentId: 'data',
-      document: { rows: rows, cols: cols, cells: cells }
-    })
   }
 
   fetchAlertStatuses(cells) {
@@ -245,6 +151,22 @@ export default class Board extends React.Component {
     })
   }
 
+  persistData(rows, cols, cells) {
+    const { board, accountId } = this.props
+
+    const data = { rows: rows, cols: cols, cells: cells }
+
+    AccountStorageMutation.mutate({
+      actionType: AccountStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
+      collection: 'neondb-' + board.id,
+      accountId: accountId,
+      documentId: 'data',
+      document: data
+    }).then(() => {
+      this.setState(data)
+    })
+  }
+
   humanizeNumber(value, style) {
     if (!value && value != 0) return ''
     value = Number(value)
@@ -279,7 +201,7 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const { data, modalHidden, rows, rowName, rowForCell, cols, colName, colForCell, cellType, policyName, attributeName, isType, valueName, board } = this.state
+    const { data, modalHidden, rows, cols, cells, board } = this.state
 
     return (
       <div>
@@ -306,63 +228,7 @@ export default class Board extends React.Component {
           <a href="#" className="default" onClick={(e) => this.openAdmin(e)}>admin</a>
         </div>
         <Modal hidden={modalHidden} onClose={this.closeAdmin}>
-          <div>
-            <h3>Board Details</h3>
-            <Tabs>
-              <TabsItem itemKey="tab-rows" label="Rows">
-                <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
-                  <TextField label="Title" placeholder="" onChange={(e) => this.textUpdated(e, 'row')} value={rowName} />
-                  <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('row')}>Add</Button>
-                </div>
-              </TabsItem>
-              <TabsItem itemKey="tab-cols" label="Columns">
-                <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
-                  <TextField label="Title" placeholder="" onChange={(e) => this.textUpdated(e, 'col')} value={colName} />
-                  <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('col')}>Add</Button>
-                </div>
-              </TabsItem>
-              <TabsItem itemKey="tab-cells" label="Cells">
-                <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '1em', marginTop: '2em'}}>
-                  <Select label="Row" placeholder="" values={rowForCell || []} options={rows.map(r => ({value: r}))} onChange={(val) => this.cellSelect(val, 'row')} />
-                  <Select label="Column" placeholder="" values={colForCell || []} options={cols.map(c => ({value: c}))} onChange={(val) => this.cellSelect(val, 'col')} />
-                  <ul class="segmented-control">
-                      <li class="segmented-control__item">
-                          <input class="segmented-control__input" type="radio" name="cell-alert" value="alert" checked={cellType === 'alert'} />
-                          <label class="segmented-control__label" for="cell-alert"  onClick={(e) => this.optionChange('alert', 'cell')}>NR Alert</label>
-                      </li>
-                      <li class="segmented-control__item">
-                          <input class="segmented-control__input" type="radio" name="cell-data" value="data" checked={cellType === 'data'} />
-                          <label class="segmented-control__label" for="cell-data" onClick={(e) => this.optionChange('data', 'cell')}>NR Data</label>
-                      </li>
-                  </ul>
-                  {cellType === 'alert' && <TextField label="Alert Policy" placeholder="" onChange={(e) => this.textUpdated(e, 'policy')} value={policyName} />}
-                  {cellType === 'data' && (
-                    <div>
-                    <TextField label="Attribute Name" placeholder="" onChange={(e) => this.textUpdated(e, 'attribute')} value={attributeName} />
-                    <div style={{display: 'grid', gridTemplateColumns: '3fr 1fr', gridGap: '.5em', alignItems: 'center'}}>
-                      <ul class="segmented-control">
-                          <li class="segmented-control__item">
-                              <input class="segmented-control__input" type="radio" name="is-less" value="alert" checked={isType === 'less'} />
-                              <label class="segmented-control__label" for="is-more"  onClick={(e) => this.optionChange('less', 'is')}>less than</label>
-                          </li>
-                          <li class="segmented-control__item">
-                              <input class="segmented-control__input" type="radio" name="is-equal" value="equal" checked={isType === 'equal'} />
-                              <label class="segmented-control__label" for="is-equal" onClick={(e) => this.optionChange('equal', 'is')}>equals</label>
-                          </li>
-                          <li class="segmented-control__item">
-                              <input class="segmented-control__input" type="radio" name="is-more" value="alert" checked={isType === 'more'} />
-                              <label class="segmented-control__label" for="is-more"  onClick={(e) => this.optionChange('more', 'is')}>greater than</label>
-                          </li>
-                      </ul>
-                      <TextField label="Value" placeholder="" onChange={(e) => this.textUpdated(e, 'value')} value={valueName} style={{marginBottom: '.75em'}} />
-                    </div>
-                    </div>
-                  )}
-                  <Button iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS} onClick={() => this.addData('cell')}>Add</Button>
-                </div>
-              </TabsItem>
-            </Tabs>
-          </div>
+          <BoardAdmin rows={rows} cols={cols} cells={cells} onSave={this.persistData} />
         </Modal>
       </div>
     )
