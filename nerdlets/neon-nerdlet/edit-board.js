37802458a14bbb2064857e19d9ee4c9fb924c2c7
Generosa Litton <glitton@newrelic.com>
2020-01-08 15:23:52 -0800

    added edit title component

--- nerdlets/neon-nerdlet/edit-board.js
+++ nerdlets/neon-nerdlet/edit-board.js
@@ -1,63 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import EditTitle from './edit-title';
 
-import { Tabs, TabsItem, TextField, Button, Grid, GridItem } from 'nr1';
+import { Tabs, TabsItem } from 'nr1';
 
 export default class EditBoard extends React.Component {
   static propTypes = {
     rows: PropTypes.array,
     cols: PropTypes.array,
-    onSave: PropTypes.func,
   };
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      rowName: '',
-      rowForCell: '',
-      colName: '',
-      colForCell: '',
-      cells: [],
-      cellType: '',
-      policyName: '',
-      attributeName: '',
-      isType: '',
-      editIndex: -1,
-      value: '',
-      editMode: false,
-    };
-
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.persistData = this.persistData.bind(this);
-  }
-
-  persistData(rows, cols, cells) {
-    const { onSave } = this.props;
-    if (onSave) onSave(rows, cols, cells);
-  }
-
-  toggleEdit(row) {
-    const { editMode } = this.state;
-    console.log(row);
-    this.setState({ editMode: !editMode });
-    // reference this https://codepen.io/saoirsezee/pen/yOrVra
+    // this.state = {
+    //   rowName: '',
+    //   rowForCell: '',
+    //   colName: '',
+    //   colForCell: '',
+    //   cells: [],
+    //   cellType: '',
+    //   policyName: '',
+    //   attributeName: '',
+    //   isType: '',
+    // };
   }
 
   render() {
-    const {
-      rowName,
-      rowForCell,
-      colName,
-      colForCell,
-      cellType,
-      policyName,
-      attributeName,
-      isType,
-      value,
-      editMode,
-    } = this.state;
-    const { rows, cols, cells } = this.props;
+    // const {
+    //   rowName,
+    //   rowForCell,
+    //   colName,
+    //   colForCell,
+    //   cellType,
+    //   policyName,
+    //   attributeName,
+    //   isType,
+    // } = this.state;
+    const { rows, cols } = this.props;
 
     return (
       <div>
@@ -72,36 +52,7 @@ export default class EditBoard extends React.Component {
                 marginTop: '2em',
               }}
             >
-              {editMode ? (
-                <div>
-                  {rows.map((row, i) => (
-                    <div key={i}>
-                      <TextField label="Title" value={row} />
-                      <Button
-                        iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__EDIT}
-                        onClick={() => this.toggleEdit(row)}
-                      >
-                        Done
-                      </Button>
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <div>
-                  {rows.map((row, i) => (
-                    <div key={i}>
-                      <TextField label="Title" value={row} />
-
-                      <Button
-                        iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__EDIT}
-                        onClick={() => this.toggleEdit(row)}
-                      >
-                        Edit
-                      </Button>
-                    </div>
-                  ))}
-                </div>
-              )}
+              <EditTitle rows={rows} />
             </div>
           </TabsItem>
           <TabsItem value="tab-cols" label="Columns">
@@ -114,27 +65,7 @@ export default class EditBoard extends React.Component {
               }}
             >
               <div>
-                {cols.map((col, i) => (
-                  <Grid key={i}>
-                    <GridItem columnSpan={4}>
-                      <div>{col}</div>
-                    </GridItem>
-                    <GridItem columnSpan={4}>
-                      <Button
-                        iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__EDIT}
-                      >
-                        Edit
-                      </Button>
-                    </GridItem>
-                    <GridItem columnSpan={4}>
-                      <Button
-                        iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__EDIT}
-                      >
-                        Save
-                      </Button>
-                    </GridItem>
-                  </Grid>
-                ))}
+                <EditTitle cols={cols} />
               </div>
             </div>
           </TabsItem>
