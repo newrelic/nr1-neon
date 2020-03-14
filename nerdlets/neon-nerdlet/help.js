36b19c8d64857d68fb6ed23a5d50bf2f857f6ff8
Generosa Litton <glcodeworks@gmail.com>
2020-03-14 16:47:09 -0700

    Added links to getting started sections

--- nerdlets/neon-nerdlet/help.js
+++ nerdlets/neon-nerdlet/help.js
@@ -9,14 +9,14 @@ export default class Help extends React.Component {
       newBoardModalHidden: true,
     };
     this._onClose = this._onClose.bind(this);
-    this.openCreateNewBoard = this.openCreateNewBoard.bind(this);
+    this.openHelp = this.openHelp.bind(this);
   }
 
   _onClose() {
     this.setState({ newBoardModalHidden: true });
   }
 
-  openCreateNewBoard(e) {
+  openHelp(e) {
     e.preventDefault();
     this.setState({ newBoardModalHidden: false });
   }
@@ -27,19 +27,40 @@ export default class Help extends React.Component {
       <div>
         <Dropdown title="Getting Started">
           <DropdownItem>
-            <a href="#" onClick={e => this.openCreateNewBoard(e)}>
+            <a href="#create_new_board" onClick={e => this.openHelp(e)}>
               Create a New Board
             </a>
           </DropdownItem>
-          <DropdownItem>Set Up Board</DropdownItem>
-          <DropdownItem>View Boards</DropdownItem>
-          <DropdownItem>Edit Board</DropdownItem>
-          <DropdownItem>Delete Board</DropdownItem>
+          <DropdownItem>
+            <a href="#set_up_board" onClick={e => this.openHelp(e)}>
+              Set Up Board
+            </a>
+          </DropdownItem>
+          <DropdownItem>
+            <a href="#view_boards" onClick={e => this.openHelp(e)}>
+              View Boards
+            </a>
+          </DropdownItem>
+          <DropdownItem>
+            <a href="#edit_board" onClick={e => this.openHelp(e)}>
+              Edit Board
+            </a>
+          </DropdownItem>
+          <DropdownItem>
+            <a href="#delete_board" onClick={e => this.openHelp(e)}>
+              Delete Board
+            </a>
+          </DropdownItem>
         </Dropdown>
 
         <Modal hidden={newBoardModalHidden} onClose={this._onClose}>
           <div>
-            Create A New Board
+            <h1>Getting Started</h1>
+            <h3 id="create_new_board">Create A New Board</h3>
+            <h3 id="set_up_board">Set Up Board</h3>
+            <h3 id="view_boards">View Boards</h3>
+            <h3 id="edit_board">Edit Board</h3>
+            <h3 id="delete_board">Delete Board</h3>
             <Button onClick={this._onClose}>Close</Button>
           </div>
         </Modal>
