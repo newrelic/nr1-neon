b525c1bf92467e2fdb8233150ffcfbfdf5d25ab6
Generosa Litton <glcodeworks@gmail.com>
2020-03-19 16:49:59 -0700

    added smaller screenshots for docs

--- nerdlets/help/index.js
+++ nerdlets/help/index.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { Grid, GridItem, Stack, StackItem, Link } from 'nr1';
+import logo from '../../catalog/screenshots/logo.png';
+import neon1 from '../../catalog/screenshots/neon1.png';
+import neon2 from '../../catalog/screenshots/neon2.png';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class HelpNerdlet extends React.Component {
   render() {
-    return <h1>Hello, help Nerdlet!</h1>;
+    return (
+      <div>
+        <Stack
+          className="toolbar-container"
+          fullWidth
+          gapType={Stack.GAP_TYPE.NONE}
+          horizontalType={Stack.HORIZONTAL_TYPE.FILL_EVENLY}
+          verticalType={Stack.VERTICAL_TYPE.FILL}
+        >
+          <StackItem className="logo-container">
+            <img className="neon-logo" src={logo} alt="Neon Logo" />
+          </StackItem>
+        </Stack>
+        <Grid
+          className="primary-grid"
+          spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+        >
+          <GridItem className="primary-content-container" columnSpan={12}>
+            <main className="primary-content full-height">
+              <h1 className="doc-title">Neon Documentation</h1>
+              <ul className="docs-section">
+                <li>
+                  <h3>About Neon</h3>
+                </li>
+                <p>
+                  Neon is an application that allows you to create a status at a
+                  glance visualization that can be scaled to track the health of
+                  entire business units or regions. Statuses are derived from
+                  existing New Relic alert policies or based on values from New
+                  Relic events. Neon makes it easy to configure the
+                  visualization to show exactly what you need to see.
+                </p>
+                <div className="screenshots">
+                  <img
+                    className="fulfillment-screen"
+                    src={neon1}
+                    alt="Neon Board"
+                  />
+                  <img
+                    className="boards-screen"
+                    src={neon2}
+                    alt="Neon Boards"
+                  />
+                </div>
+
+                <br />
+                <li>
+                  <h3>Dependencies</h3>
+                </li>
+                <p>
+                  To use Neon to monitor your alerts, you need to have New Relic
+                  Alerts and a webhook notification channel set up. Instructions
+                  for webhook setup is{' '}
+                  <Link to="https://github.com/glitton/nr1-neon/blob/documentation/docs/alert_webhook_config.md">
+                    here.
+                  </Link>
+                </p>
+              </ul>
+            </main>
+          </GridItem>
+        </Grid>
+      </div>
+    );
   }
 }
---
+++ nerdlets/neon-nerdlet/help-draft.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+  Dropdown,
+  DropdownItem,
+  Modal,
+  Button,
+  HeadingText,
+  BlockText,
+  Link,
+} from 'nr1';
+
+export default class Help extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newBoardModalHidden: true,
+    };
+    this._onClose = this._onClose.bind(this);
+    this.openHelp = this.openHelp.bind(this);
+  }
+
+  _onClose() {
+    this.setState({ newBoardModalHidden: true });
+  }
+
+  openHelp(e) {
+    e.preventDefault();
+    this.setState({ newBoardModalHidden: false });
+  }
+
+  render() {
+    const { newBoardModalHidden } = this.state;
+    return (
+      <div>
+        <Dropdown title="Getting Started">
+          <DropdownItem href="#dependencies" onClick={e => this.openHelp(e)}>
+            Dependencies
+          </DropdownItem>
+          <DropdownItem
+            href="#create_new_board"
+            onClick={e => this.openHelp(e)}
+          >
+            Create a New Board
+          </DropdownItem>
+          <DropdownItem href="#set_up_board" onClick={e => this.openHelp(e)}>
+            Set Up Board
+          </DropdownItem>
+          <DropdownItem href="#view_boards" onClick={e => this.openHelp(e)}>
+            View Boards
+          </DropdownItem>
+          <DropdownItem href="#edit_board" onClick={e => this.openHelp(e)}>
+            Edit Board
+          </DropdownItem>
+          <DropdownItem href="#delete_board" onClick={e => this.openHelp(e)}>
+            Delete Board
+          </DropdownItem>
+        </Dropdown>
+
+        <Modal hidden={newBoardModalHidden} onClose={this._onClose}>
+          <div>
+            <HeadingText type={HeadingText.TYPE.HEADING_3}>
+              Getting Started With Neon
+            </HeadingText>
+            <h3 id="dependencies">Dependencies</h3>
+            <BlockText type={BlockText.TYPE.PARAGRAPH}>
+              You need to have New Relic Alerts and a webhook notification
+              channel set up. Instructions for webhook setup is{' '}
+              <Link to="https://github.com/glitton/nr1-neon/blob/documentation/docs/alert_webhook_config.md">
+                here.
+              </Link>
+            </BlockText>
+            <h3 id="create_new_board">Create A New Board</h3>
+            <BlockText type={BlockText.TYPE.PARAGRAPH}>
+              <li>Click the plus icon (+)</li>
+              <li>
+                In the <strong>BOARD NAME</strong> text field, type in a name
+                for your board
+              </li>
+              <li>Click the plus icon (+)</li>
+            </BlockText>
+            <h3 id="set_up_board">Set Up Board</h3>
+            <h3 id="view_boards">View Boards</h3>
+            <h3 id="edit_board">Edit Board</h3>
+            <h3 id="delete_board">Delete Board</h3>
+            <Button onClick={this._onClose}>Close</Button>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
