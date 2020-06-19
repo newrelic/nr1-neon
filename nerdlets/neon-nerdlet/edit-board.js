0843d27a29fd1fa3f67d0f11a8fcca58e125aacc
Generosa Litton <glcodeworks@gmail.com>
2020-06-19 10:31:13 -0700

    editing cells wip

--- nerdlets/neon-nerdlet/edit-board.js
+++ nerdlets/neon-nerdlet/edit-board.js
@@ -8,6 +8,7 @@ export default class EditBoard extends React.Component {
   static propTypes = {
     rows: PropTypes.array,
     cols: PropTypes.array,
+    cells: PropTypes.array,
     onDataDelete: PropTypes.func,
     onDataSave: PropTypes.func,
   };
@@ -21,7 +22,7 @@ export default class EditBoard extends React.Component {
 
     return (
       <div>
-        <h3>Edit Board Titles</h3>
+        <h3>Edit Board</h3>
         <Tabs>
           <TabsItem value="tab-rows" label="Rows">
             <div
@@ -59,6 +60,27 @@ export default class EditBoard extends React.Component {
                   title={col}
                   index={i}
                   type={'cols'}
+                  onDataDelete={onDataDelete}
+                  onDataSave={onDataSave}
+                />
+              ))}
+            </div>
+          </TabsItem>
+          <TabsItem value="tab-cells" label="Cells">
+            <div
+              style={{
+                display: 'grid',
+                gridTemplateColumns: '1fr',
+                gridGap: '1em',
+                marginTop: '2em',
+              }}
+            >
+              {cells.map((cell, i) => (
+                <Title
+                  key={i}
+                  title={cell}
+                  index={i}
+                  type={'cells'}
                   onDataDelete={onDataDelete}
                   onDataSave={onDataSave}
                 />
